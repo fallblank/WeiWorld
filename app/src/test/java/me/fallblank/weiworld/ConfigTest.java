9edaf5d8a049ebdd2902fbf0c0f3696cec30681e
fallblank <fallblank525@gmail.com>
2017-03-03 11:20:05 +0800

    add scope

--- app/src/test/java/me/fallblank/weiworld/ConfigTest.java
+++ app/src/test/java/me/fallblank/weiworld/ConfigTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by fallb on 2017/3/2.
+ * Testing whether can get private information or not
  */
 
 public class ConfigTest {
@@ -15,4 +16,18 @@ public class ConfigTest {
         assertNotNull(BuildConfig.APP_KEY);
         assertNotEquals(BuildConfig.APP_KEY,"");
     }
+
+    @Test
+    public void canGetSecret(){
+        assertNotNull(BuildConfig.APP_SECRET);
+        assertNotEquals(BuildConfig.APP_SECRET,"");
+    }
+
+    @Test
+    public void canGetScope(){
+        assertNotNull(BuildConfig.APP_SCOPE);
+        assertNotEquals(BuildConfig.APP_SCOPE,"");
+    }
+
+
 }
